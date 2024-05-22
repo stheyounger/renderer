2ef7be3a4dc524942abd81dd8572f7cfb27d3bfc
stheyounger <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>
2024-05-22 07:59:12 -0700

    Initial Commit

---
+++ renderer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  renderer
+//
+//  Created by James Penrose on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ renderer/rendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  rendererApp.swift
+//  renderer
+//
+//  Created by James Penrose on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct rendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
